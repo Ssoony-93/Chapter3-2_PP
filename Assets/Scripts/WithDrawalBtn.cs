0a03eb80752158dec7086e3e3d024be47e05be6d
Ssoony-93 <tnsrn93@gmail.com>
2023-12-13 20:50:52 +0900

    231213_lose

--- Assets/Scripts/WithDrawalBtn.cs
+++ Assets/Scripts/WithDrawalBtn.cs
@@ -2,11 +2,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-
-public class DButtonClick : MonoBehaviour
+public class WButtonClick : MonoBehaviour
 {
-
-    // 버튼 클릭 시 호출될 함수
+       
     public void OnButtonClick()
     {
         // 디버그 테스트
@@ -15,7 +13,7 @@ public class DButtonClick : MonoBehaviour
 
 
         //Scene전환!
-        SceneManager.LoadScene("DepositScene");
+        SceneManager.LoadScene("WithDrawalScene");
     }
-
+ 
 }
