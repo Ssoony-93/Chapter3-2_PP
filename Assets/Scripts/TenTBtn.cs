9e117baf20c4cdc6c4d72944f3a5c5999e5cd2d4
Ssoony-93 <tnsrn93@gmail.com>
2023-12-13 22:15:26 +0900

    231213_uu

--- Assets/Scripts/TenTBtn.cs
+++ Assets/Scripts/TenTBtn.cs
@@ -1,17 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-
-public class DButtonClick : MonoBehaviour
+public class TenTBtn : MonoBehaviour
 {
-
-    // 버튼 클릭 시 호출될 함수
     public void OnButtonClick()
     {
-    
-        
-    
-    }
 
+
+        
+    }
 }
